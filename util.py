f0115c77cb6e744b606ecae21b8cb0c3c1f0c141
Aran Miller <aranjmiller@gmail.com>
2020-06-22 15:14:58 -0700

    updated default power

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
-def nth_power(n, fn = lambda x: x**2):
+def nth_power(n, fn = lambda x: x**3):
     #calcualtes power for number upto n
     #args:
         #n: highest number in the list of numbers
-        #power: power for numbers to raise, feafault pwoer is 2
+        #power: power for numbers to raise, feafault pwoer is 3
     return [fn(i) for i in range(n)]
 
 
